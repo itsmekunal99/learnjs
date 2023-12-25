218e1315100d49830e6d3ee521cac3c1993ae614
Kunal <kunal99xcross@gmail.com>
2023-12-25 17:21:51 +0530

    2

---
+++ 03datatype.js
@@ -0,0 +1,18 @@
+"use script" ;// treats all js as newer version
+// console.log("first") ; console.log("second ") ;  //this is not good habbit of coding
+
+// there are many data types but we use them using let keyword only
+
+let name ="harish"       // string 
+let age = 45             // number
+let ismarried = false    // boolean
+// null =>standalone value
+// undefined => when value is not assigned
+// symbol =>unique values
+
+// typeof
+console.log(typeof age)          // returns number
+console.log(typeof name)         // returns string
+console.log(typeof ismarried)    // returns bollean
+console.log(typeof null )        // returns object
+console.log(typeof undefined )   // returns undefined
